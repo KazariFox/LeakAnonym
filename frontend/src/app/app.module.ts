a4a76c93060eed01524a4347779a327990252458
xareyli <xareyli@gmail.com>
2023-11-25 21:07:21 +0300

    chore: add index file to auth module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,13 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { LeanAuthModule } from "@lean/lean-auth";
+import { environment } from '../environments/environment.prod';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LeanAuthModule.forRoot({
+      authApi: environment.apiUrl + 'auth',
+      registerApi: environment.apiUrl + 'register',
+      renewApi: environment.apiUrl + 'renew'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/libs/lean-auth/index.ts
@@ -0,0 +1,6 @@
+export * from './lean-auth.module';
+export * from './services/auth.service';
+export * from './interfaces/auth-options.interface';
+export * from './interfaces/login-dto.interface';
+export * from './interfaces/register-dto.interface';
+export * from './guards/auth.guard';
